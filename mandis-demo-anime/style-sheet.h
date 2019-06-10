ff9b4c7ab42a7a8b2b42154d0c62d4cf8d8e0e2b
Xxiong <xxiong@cqu.edu.cn>
2019-06-10 21:50:43 +0800

    style sheets

---
+++ mandis-demo-anime/style-sheet.h
@@ -0,0 +1,41 @@
+#pragma once
+
+const char * SETTINGS_BUTTON_STYLE_SHEET = "\
+QPushButton {\
+    background-color:rgba(60,60,60,5);\
+    border-radius:30px;\
+    font-size:20px;\
+    color:rgba(0,0,0,30);\
+}\
+QPushButton:hover {\
+    background-color:rgba(60,60,60,30);\
+    font-size:25px;\
+    color:rgba(0,0,0,100);\
+}\
+QPushButton:pressed {\
+    background-color:rgba(60,60,60,90);\
+    color:rgba(0,0,0,255);\
+    border-width:1;\
+    border-color:rgba(60,60,60,255);\
+    border-style:solid;\
+}";
+
+const char * START_BUTTON_STYLE_SHEET = "\
+QPushButton {\
+    background-color:rgba(60,60,60,5);\
+    border-radius:30px;\
+    font-size:20px;\
+    color:rgba(0,0,0,30);\
+}\
+QPushButton:hover {\
+    background-color:rgba(60,60,60,30);\
+    font-size:25px;\
+    color:rgba(0,0,0,100);\
+}\
+QPushButton:pressed {\
+    background-color:rgba(60,60,60,90);\
+    color:rgba(0,0,0,255);\
+    border-width:1;\
+    border-color:rgba(60,60,60,255);\
+    border-style:solid;\
+}";
